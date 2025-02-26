8a469a1bd505dc8ffc77b15d09eadec710790f42
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2025-02-26 18:06:49 +1000

    nForm-ru

--- src/utils.ts
+++ src/utils.ts
@@ -67,12 +67,12 @@ export const parseVotes = (
 export function nFormatter(num: number, digits = 2) {
   const lookup = [
     { value: 1, symbol: "" },
-    { value: 1e3, symbol: "K" },
-    { value: 1e6, symbol: "M" },
-    { value: 1e9, symbol: "G" },
-    { value: 1e12, symbol: "T" },
-    { value: 1e15, symbol: "P" },
-    { value: 1e18, symbol: "E" },
+    { value: 1e3, symbol: " тыс." },
+    { value: 1e6, symbol: " млн." },
+    { value: 1e9, symbol: " млрд." },
+    { value: 1e12, symbol: " трлн." },
+    { value: 1e15, symbol: " квадрлн." },
+    { value: 1e18, symbol: " квинтлн." },
   ];
   const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
   var item = lookup
