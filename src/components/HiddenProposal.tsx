507d6b95b99d3dedeb53ba4221882b487789c7e6
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2025-03-04 10:52:28 +1000

    st_con_hide

--- src/components/HiddenProposal.tsx
+++ src/components/HiddenProposal.tsx
@@ -16,7 +16,7 @@ export const HiddenProposal = ({
   }
   return (
     <StyledContainer className={className}>
-      <AppTooltip text="This proposal is hidden">
+      <AppTooltip text="Это предложение скрыто">
         <AiFillEyeInvisible />
       </AppTooltip>
     </StyledContainer>
