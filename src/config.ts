2418807cdb49247d9feb6640d2c0a8763c75a0c1
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2025-05-19 15:53:00 +1000

    Telegram Analytics is a powerful SDK and API that enables your mini-application to become a rich source of actionable data. By tracking user activity within your app, it transforms that data into clear, useful analytics that can help you optimize your application and enhance user engagement.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
 import _ from "lodash";
 import { ReleaseMode, VotingPowerStrategyType } from "ton-vote-contracts-sdk";
 import { StrategyOption } from "types";
+import TelegramAnalytics from "@telegram-apps/analytics";
+
+TelegramAnalytics.init({
+  token:
+    "eyJhcHBfbmFtZSI6IkdyYWRvc3BoZXJhX1ZvdGUiLCJhcHBfdXJsIjoiaHR0cHM6Ly90Lm1lL2dyYWRvc3BoZXJhX3ZvdGVfYm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vdm90ZS5ncmFkb3NwaGVyYS5vcmcvRVFERXhscDZFamtWTi1PSjFaY0dMRVlsSVRhb2xkNXl0QlAzZThnNmdfQklSYVpYIn0=!/ZVr7tFUH54VuCCAPRZimip/DlmF7/U+E2lM4/hQeIo=",
+  appName: "Gradosphera_Vote",
+});
+
 export const TONVIEWER = "https://tonviewer.com";
 export const TONVIEWER_ADDRESS_URL = `${TONVIEWER}/`;
 export const APP_NAME = "Голос";
