f4e34f22c13c7bed3d42a253d109b26ba042b879
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2025-05-19 18:50:21 +1000

    _tg_analytics

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,13 @@ import { manifestUrl } from "config";
 import { clearAllToasts } from "toasts";
 import { useSettingsStore } from "store";
 import { TonConnectInitializer } from "components/TonConnectInitializer";
+import TelegramAnalytics from '@telegram-apps/analytics'
 
+TelegramAnalytics.init({
+  token:
+    "eyJhcHBfbmFtZSI6IkdyYWRvc3BoZXJhX1ZvdGUiLCJhcHBfdXJsIjoiaHR0cHM6Ly90Lm1lL2dyYWRvc3BoZXJhX3ZvdGVfYm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vdm90ZS5ncmFkb3NwaGVyYS5vcmcvRVFERXhscDZFamtWTi1PSjFaY0dMRVlsSVRhb2xkNXl0QlAzZThnNmdfQklSYVpYIn0=!/ZVr7tFUH54VuCCAPRZimip/DlmF7/U+E2lM4/hQeIo=",
+  appName: "Gradosphera_Vote",
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
