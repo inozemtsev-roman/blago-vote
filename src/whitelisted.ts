ed44928a4f57c1484ab220e3c539926d83ed5c15
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2026-05-29 20:02:41 +1000

    add verifier DAO _KHV

--- src/whitelisted.ts
+++ src/whitelisted.ts
@@ -1,6 +1,7 @@
 import { IS_DEV } from "config";
 
 export const WHITELISTED_DAOS: string[] = [
-  "EQDExlp6EjkVN-OJ1ZcGLEYlITaold5ytBP3e8g6g_BIRaZX"
-]
-export const WHITELISTED_PROPOSALS: string[] = []
+  "EQDExlp6EjkVN-OJ1ZcGLEYlITaold5ytBP3e8g6g_BIRaZX",
+  "EQAW6n5B029HoOFkRFeMXyGDvUBF6gbVrS6bnNYJ6aWv_y1S",
+];
+export const WHITELISTED_PROPOSALS: string[] = [];
